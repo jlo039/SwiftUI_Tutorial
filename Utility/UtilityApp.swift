e27ce287b823f159e3fbe1195acec4e0bc14126a
Jason Lott <jasoncom566@gmail.com>
2023-10-26 16:06:57 -0400

    Initial Commit

---
+++ Utility/UtilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  UtilityApp.swift
+//  Utility
+//
+//  Created by Jason Samuel Lott on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct UtilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
